611959bed25849c3d1427837e56892a0348cfe9b
revanurlestari <revanurlestari8@gmail.com>
2026-05-30 16:55:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -584,7 +584,7 @@ dan gaya tarik antar molekul.
         T = st.number_input("T", value=0.0)
         V = st.number_input("V", value=0.0)
         a = st.number_input("a", value=0.0)
-        b = st.number_input("b", value=0.0)
+        b = st.number_input("b", value=0.000)
 
         if st.button("Hitung"):
 
